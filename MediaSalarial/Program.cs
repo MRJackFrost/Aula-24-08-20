bb3850da6ccd6cc99399473931efc758701344cb
MRJackFrost <69490107+MRJackFrost@users.noreply.github.com>
2020-09-07 18:24:27 -0300

    Add files via upload

---
+++ MediaSalarial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace MediaSalarial
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario func1, func2;
+            func1 = new Salario();
+            Console.WriteLine("Qual o nome do primeiro funcionario");
+            func1.nome = Console.ReadLine();
+            Console.WriteLine("Qual o salario do primeiro funcionario");
+            func1.salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            func2 = new Salario();
+            Console.WriteLine("Qual o nome do segundo funcionario");
+            func2.nome = Console.ReadLine();
+            Console.WriteLine("Qual o salario do segundo funcionario");
+            func2.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double media = (func2.salario + func1.salario) / 2;
+
+            Console.WriteLine("A media salarial dos funcionarios "+func2.nome+" e "+func1.nome+" é: "+media);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PessoaIdade/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Pessoa.Idade{
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa persona1, persona2;
+            persona1 = new Pessoa();
+            persona2 = new Pessoa();
+            Console.WriteLine("Qual o nome da primeira pessoa");
+            persona1.nome = Console.ReadLine();
+            Console.WriteLine("Qual a idade da primeira pessoa");
+            persona1.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Qual o nome da segunda pessoa");
+            persona2.nome = Console.ReadLine();
+            Console.WriteLine("Qual a idade da segunda pessoa");
+            persona2.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (persona1.idade > persona2.idade)
+            {
+                Console.WriteLine(persona1.nome + " é a pessoa mais velha");
+            }
+            else if(persona1.idade == persona2.idade) {
+                Console.WriteLine(persona2.nome + " e " + persona1.nome + " tem a mesma idade");
+            }
+            else {
+                Console.WriteLine(persona2.nome + " é a pessoa mais velha");
+            }
+            Console.ReadKey();
+        }
+    }
+}
